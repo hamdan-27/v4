be63f1c8e43c5fd79bcc93ab88caf4dbe0d21bac
Hamdan Mohammad <hamdan102703@gmail.com>
2025-06-05 18:35:13 +0400

    changed loader and about section

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,19 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'Python',
+    'LangChain',
+    'FastAPI',
+    'PostgreSQL',
+    'Flask',
+    'MySQL',
+    'React',
+    'MongoDB',
+    'Node.js',
+    'AWS',
+    'Express.js',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,31 +147,26 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hey there! I&apos;m Hamdan, a driven software engineering student, passionate about AI
+              and web development with hands-on experience gained through internships and personal
+              projects.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              My technical toolkit spans Python, JavaScript, and DevOps, allowing me to tackle
+              complex challenges in backend development, NLP, and ML. I&apos;m eager to leverage my
+              diverse skill set to create innovative technological solutions and make meaningful
+              contributions in the AI and software engineering landscape.
             </p>
 
-            <p>
+            {/* <p>
               I also recently{' '}
               <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
                 launched a course
               </a>{' '}
               that covers everything you need to build a web app with the Spotify API using Node
               &amp; React.
-            </p>
+            </p> */}
 
             <p>Here are a few technologies I’ve been working with recently:</p>
           </div>
